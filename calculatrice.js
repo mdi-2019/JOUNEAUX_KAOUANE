1b6490fa0ba9b06228b90e0190df9d0b866f6635
Gwendal <gwendal.jouneaux@etudiant.univ-rennes1.fr>
2019-02-11 16:47:13 +0100

    Add the mode choice
    Change the calculatrice.js to ask a mode rather than just print Hello
    
    This commit does the following :
    * Add "readline" lib
    * Create a recursive function to ask the mode
    * Save the mode in a variable named "mode"

--- calculatrice.js
+++ calculatrice.js
@@ -1,3 +1,27 @@
-require("util");
+// Lib includes
+const readline = require('readline');
 
-console.log("Hello");
+// Constant
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Variables
+var mode = 0
+
+//Function
+var chooseMode = function () {
+    rl.question('Which mode do you want to use :\n1. Basic mode\n2. Scientific mode\n', (answer) => {
+	  mode = answer;
+	  if(mode != "1" && mode != "2"){
+	  	chooseMode(); //Calling this function again to ask new question
+	  } else {
+	  	console.log("You choose the mode : " + (mode=="1"?"Basic":"scientific"));
+	  	rl.close();
+	  }
+	});
+};
+
+//MAIN CODE
+chooseMode();
